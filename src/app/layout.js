b36dadf61bf2d2c12df34e18e70a617cb4c4d5fc
Youcef Shaath <oy_shaath@esi.dz>
2026-09-13 19:24:12 +0100

    add fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,24 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const consolas = localFont({
+  src: [
+    { path: '../../public/fonts/consolas-regular.ttf', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/consolas-bold.ttf', weight: '700', style: 'normal' },
+    { path: '../../public/fonts/consolas-italic.ttf', weight: '400', style: 'italic' },
+    { path: '../../public/fonts/consolas-bold-italic.ttf', weight: '700', style: 'italic' },
+  ],
+  variable: '--font-consolas',
+  display: 'swap',
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const haettenschweiler = localFont({
+  src: '../../public/fonts/haettenschweiler.ttf',
+  variable: '--font-haettenschweiler',
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning
+      className={`${haettenschweiler.variable} ${consolas.variable} h-full antialiased`} suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
