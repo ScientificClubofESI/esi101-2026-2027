fad9e54e2b745bb925ba27b24a15c0b9144b0eb7
yasser lakrid <yasserlakrid36@gmail.com>
2026-09-15 17:30:58 +0100

    initializing the navbar

--- src/components/NavBar/page.jsx
+++ src/components/NavBar/page.jsx
@@ -1,18 +1,44 @@
+"use client"
 import React from "react";
 import { useTheme } from "next-themes";
 import ThemeToggle from "./ThemeToggle";
+import './navbar.css'
 
 const NabBar = () => {
     const theme = useTheme();
 };
 
+const links = ["Home" , "Who's behind Esi 101" , "Ask a student" , "Still wondering ?" , "Contact"]
 
-
-const NavBar = () => {
+const NavBar = (toggleChatbot) => {
   return (
-    <div>
-      NavBar
-      <ThemeToggle />
+    <div className="navbar">
+    
+
+      <div className="links">
+          <div className="logo">
+    <img src="/assets/Logo.svg"></img>
+    
+</div>
+<div className="links-inner">
+   {links.map((link , index)=>(
+
+          <a className="font-haetten" href={link} key={index}>{link}</a>)
+          
+        )}
+</div>
+       
+<ThemeToggle />
+
+      </div>
+      
+      <div className="chatbot" onClick={()=>{toggleChatbot(true)}}>
+        <p className="font-haetten">Chat with Cissou
+          </p>
+        <img src="/assets/cissouBtn.svg"></img>
+    
+      </div>
+      
     </div>
   );
 };
