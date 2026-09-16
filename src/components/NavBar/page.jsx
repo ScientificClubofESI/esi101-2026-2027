aff76b8c109b19d89cfa06fd4c16a949777e5944
yasser lakrid <yasserlakrid36@gmail.com>
2026-09-16 20:51:28 +0100

    there is still some colors matching and the logo

--- src/components/NavBar/page.jsx
+++ src/components/NavBar/page.jsx
@@ -1,44 +1,73 @@
-"use client"
-import React from "react";
-import { useTheme } from "next-themes";
+"use client";
+
+import { useEffect, useState } from "react";
 import ThemeToggle from "./ThemeToggle";
-import './navbar.css'
+import "./navbar.css";
 
-const NabBar = () => {
-    const theme = useTheme();
-};
+const links = [
+  "Home",
+  "Who's behind Esi 101",
+  "Ask a student",
+  "Still wondering ?",
+  "Contact",
+];
 
-const links = ["Home" , "Who's behind Esi 101" , "Ask a student" , "Still wondering ?" , "Contact"]
+const NavBar = ({ toggleChatbot }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [listToggled , toggleList] = useState(false); 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1200);
+      console.log("the mobile is on:", window.innerWidth < 1200);
+    };
 
-const NavBar = (toggleChatbot) => {
+    // Set initial value
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="navbar">
-    
+      <div className= {listToggled? "links rounded " : "links"}>
+        <div className="logo">
+          <img src="/assets/Logo.svg" alt="ESI 101 logo" />
+        </div>
 
-      <div className="links">
-          <div className="logo">
-    <img src="/assets/Logo.svg"></img>
-    
-</div>
-<div className="links-inner">
-   {links.map((link , index)=>(
-
-          <a className="font-haetten" href={link} key={index}>{link}</a>)
-          
-        )}
-</div>
        
-<ThemeToggle />
 
+        <div className={listToggled ? "toggled" : "links-inner"} >
+          {links.map((link, index) => (
+            <a className="font-haetten" href={link} key={index}>
+              {link}
+
+            </a>
+          ))}
+        </div>
+
+        <ThemeToggle />
+         {isMobile && (
+          <div className="burger" onClick={()=>toggleList(prev => !prev)}>
+            {[1, 2, 3].map((item, index) => (
+              <div className={item} key={index}></div>
+            ))}
+          </div>
+        )}
       </div>
-      
-      <div className="chatbot" onClick={()=>{toggleChatbot(true)}}>
-        <p className="font-haetten">Chat with Cissou
-          </p>
-        <img src="/assets/cissouBtn.svg"></img>
-    
+
+      <div
+        className="chatbot"
+        onClick={() => toggleChatbot(true)}
+      >
+        <div>
+          <p className="font-haetten">Chat with Cissou</p>
+          <img src="/assets/cissouBtn.svg" alt="Chat with Cissou" />
+        </div>
       </div>
-      
     </div>
   );
 };
