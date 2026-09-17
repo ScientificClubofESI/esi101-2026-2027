bfdbfe718a1e7a3724527811eb4614ecac509021
Youcef Shaath <oy_shaath@esi.dz>
2026-09-17 17:07:32 +0100

    fixing theme toggle icons and navbar position

--- src/components/NavBar/ThemeToggle.jsx
+++ src/components/NavBar/ThemeToggle.jsx
@@ -21,12 +21,12 @@ export default function ThemeToggle() {
   return (
     <button 
       onClick={() => setTheme(isDark ? "light" : "dark")}
-      className="p-1 rounded-md hover:bg-gray-800 dark:hover:bg-gray-100 transition-colors"
+      className="p-2 rounded-md hover:border-[1px] hover:border-[rgba(158, 156, 156, 0.25)] dark:hover:border-gray-600 cursor-pointer"
       aria-label={isDark ? "Switch to Light Mode" : "Switch to Dark Mode"}
     >
       <Image 
-        src={isDark ? "/assets/Sun.svg" : "/assets/Moon.svg"} 
-        alt={isDark ? "Sun Icon" : "Moon Icon"} 
+        src={isDark ? "/assets/Moon.svg" : "/assets/Sun.svg"} 
+        alt={isDark ? "Moon Icon" : "Sun Icon"} 
         width={24} 
         height={24} 
       />
